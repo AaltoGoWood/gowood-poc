b4913d95f61f73519b533a1625f151d870ff1e9a
pisketti <tero.kadenius@flowa.fi>
2019-10-31 10:27:45 +0200

    Adds a new dummy component for map search view

--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -8,16 +8,19 @@ import { Sources, Sinks, Reducer, Component } from '../interfaces';
 
 import { Counter, State as CounterState } from './counter';
 import { Speaker, State as SpeakerState } from './speaker';
+import { MapSearch, State as MapSearchState } from './map-search';
 
 export interface State {
     counter?: CounterState;
     speaker?: SpeakerState;
+    mapSearch?: MapSearchState;
 }
 
 export function App(sources: Sources<State>): Sinks<State> {
     const match$ = sources.router.define({
         '/counter': isolate(Counter, 'counter'),
-        '/speaker': isolate(Speaker, 'speaker')
+        '/speaker': isolate(Speaker, 'speaker'),
+        '/map-search': isolate(MapSearch, 'map-search')
     });
 
     const componentSinks$: Stream<Sinks<State>> = match$
@@ -31,7 +34,7 @@ export function App(sources: Sources<State>): Sinks<State> {
 
     const redirect$: Stream<string> = sources.router.history$
         .filter((l: Location) => l.pathname === '/')
-        .mapTo('/counter');
+        .mapTo('/map-search');
 
     const sinks = extractSinks(componentSinks$, driverNames);
     return {
