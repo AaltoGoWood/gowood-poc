cf8af10bc856338657de797b0b3e35a9ddf64fa8
Elias Kunnas <elias@kunnas.com>
2019-11-05 15:08:48 +0200

    Add minimal "working" map widget

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -3,6 +3,15 @@ import { getDrivers, wrapMain } from './drivers';
 import { Component } from './interfaces';
 import { App } from './components/app';
 
+import { initMap } from './components/map';
+
 const main: Component<any> = wrapMain(App);
 
 run(main as any, getDrivers());
+
+// XXX super hacky
+if (document.location.href.indexOf('map') !== -1) {
+    initMap();
+} else {
+    document.querySelector('map')!.remove();
+}
