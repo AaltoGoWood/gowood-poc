ed9a2b7755cda519ba0026b6a7300a76acfc1fa8
pisketti <tero.kadenius@flowa.fi>
2019-11-06 15:52:39 +0200

    Gets plywood details when a row is clicked in asset table

--- frontend/src/components/building.tsx
+++ frontend/src/components/building.tsx
@@ -18,23 +18,26 @@ export const defaultState: State = {
 interface DOMIntent {
     link$: Stream<null>;
     building$: Stream<any>;
+    dispatch$: Stream<any>;
 }
 
 export function Building(props: any, sources: Sources<State>): Sinks<State> {
     console.log('Building', sources);
     const { DOM, state, router, dataQuery }: Sources<State> = sources;
     const props$ = xs.of(props);
-    const { link$, building$ }: DOMIntent = intent(DOM, props$);
+    const { link$, building$, dispatch$ }: DOMIntent = intent(DOM, props$);
+
     return {
-        DOM: view(state.stream),
+        DOM: view(state.stream, dispatch$),
         state: model(building$, dataQuery),
         router: redirect(link$),
-        dataQuery: query(building$)
+        dataQuery: query(building$, dispatch$)
     };
 }
 
-function query(building$: Stream<string>): Stream<any> {
-    return building$.map(id => ({ type: 'buildings', id }));
+function query(building$: Stream<string>, dispatch$: Stream<any>): Stream<any> {
+    const buildingQuery$ = building$.map(id => ({ type: 'buildings', id }));
+    return xs.merge(buildingQuery$, dispatch$);
 }
 
 function model(
@@ -51,14 +54,18 @@ function model(
         .map(buildingId => ({ buildingId }))
         .map(addToState);
 
-    const buildingDetails$ = dataQuery.map(data =>
-        addToState({ buildingDetails: data })
-    );
+    const buildingDetails$ = dataQuery
+        .filter(res => res.req.type === 'buildings')
+        .map(data => addToState({ buildingDetails: data }));
 
-    return xs.merge(init$, buildingId$, buildingDetails$);
+    const plywoodDetails$ = dataQuery
+        .filter(res => res.req.type === 'plywood')
+        .map(data => addToState({ plywoodDetails: data }));
+
+    return xs.merge(init$, buildingId$, buildingDetails$, plywoodDetails$);
 }
 
-function view(state$: Stream<State>): Stream<VNode> {
+function view(state$: Stream<State>, dispatch$: Stream<any>): Stream<VNode> {
     const renderDetails = (rows: any[]) => {
         return (
             <div className="asset-table">
@@ -72,13 +79,22 @@ function view(state$: Stream<State>): Stream<VNode> {
                         </tr>
                     </thead>
                     <tbody>
-                        {rows.map((row: any) => (
-                            <tr data-action="navigate">
-                                <td>{row.type}</td>
-                                <td>{row.id}</td>
-                                <td>{row.producer}</td>
-                            </tr>
-                        ))}
+                        {rows.map((row: any) => {
+                            return (
+                                <tr
+                                    onclick={() =>
+                                        dispatch$.shamefullySendNext({
+                                            type: row.type,
+                                            id: row.id
+                                        })
+                                    }
+                                >
+                                    <td>{row.type}</td>
+                                    <td>{row.id}</td>
+                                    <td>{row.producer}</td>
+                                </tr>
+                            );
+                        })}
                     </tbody>
                 </table>
             </div>
@@ -90,6 +106,7 @@ function view(state$: Stream<State>): Stream<VNode> {
             <div className="building-details">
                 <h2>Building details</h2>
                 <p>Building id: {state.buildingId}</p>
+                <p>State: {JSON.stringify(state)}</p>
                 {state.buildingDetails ? '' : <p>Loading...</p>}
                 {state.buildingDetails && state.buildingDetails.found ? (
                     renderDetails(state.buildingDetails.data)
@@ -102,6 +119,8 @@ function view(state$: Stream<State>): Stream<VNode> {
 }
 
 function intent(DOM: DOMSource, props: Stream<any>): DOMIntent {
+    const dispatch$ = xs.never();
+
     const link$ = DOM.select('[data-action="navigate"]')
         .events('click')
         .mapTo(null);
@@ -110,7 +129,7 @@ function intent(DOM: DOMSource, props: Stream<any>): DOMIntent {
     const building$ = props.map((data: any) => {
         return data;
     });
-    return { link$, building$ };
+    return { link$, building$, dispatch$ };
 }
 
 function redirect(link$: Stream<any>): Stream<string> {
