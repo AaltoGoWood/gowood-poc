f0a550532d56ef1f9e6d16593f71a81b5cf78865
Ari-Pekka Lappi <aplappi@gmail.com>
2019-11-07 14:50:09 +0200

    Navigate to /raw-material-map when show-asset-origin dispatched

--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -4,7 +4,7 @@ import { extractSinks } from 'cyclejs-utils';
 import isolate from '@cycle/isolate';
 
 import { driverNames } from '../drivers';
-import { Sources, Sinks, Reducer, Component } from '../interfaces';
+import { Sources, Sinks, Reducer, Component, Command } from '../interfaces';
 
 import { MapSearch, State as MapSearchState } from './map-search';
 import { Building, State as BuildingState } from './building';
@@ -12,6 +12,7 @@ import {
     RawMaterialMap,
     State as RawMaterialMapState
 } from './raw-material-map';
+import { Dictionary } from 'ramda';
 
 export interface State {
     mapSearch?: MapSearchState;
@@ -24,7 +25,7 @@ export function App(sources: Sources<State>): Sinks<State> {
     sources.commandGateway = commandGateway$;
 
     const match$ = sources.router.define({
-        '/map-search': isolate(MapSearch, 'map-search'),
+        '/browse-building': isolate(MapSearch, 'map-search'),
         '/building/:id': (props: any) =>
             isolate(Building.bind(undefined, props), 'building'),
         '/raw-material-map': isolate(RawMaterialMap, 'raw-material-map')
@@ -32,7 +33,7 @@ export function App(sources: Sources<State>): Sinks<State> {
 
     const layout$ = sources.router
         .define({
-            '/map-search': { map: true, building: false },
+            '/browse-building': { map: true, building: false },
             '/building/:id': { map: false, building: true },
             '/raw-material-map': { map: true, building: false }
         })
@@ -49,16 +50,20 @@ export function App(sources: Sources<State>): Sinks<State> {
 
     const redirect$: Stream<string> = sources.router.history$
         .filter((l: Location) => l.pathname === '/')
-        .mapTo('/map-search');
+        .mapTo('/browse-building');
 
     // Ensure that first page loads are routed and rendered correctly
     const firstTimePageLoad$: Stream<string> = sources.router.history$
         .filter((l: any) => l.pathname !== '/' && l.type === undefined)
         .map((l: Location) => l.pathname);
 
+    const navigateTo: Dictionary<string> = {
+        'navigate-to-building-browser': '/browse-building',
+        'show-asset-origin': '/raw-material-map'
+    };
     const handledNavigateEvents$ = commandGateway$
-        .filter(cmd => cmd.type === 'navigate-to-building-browser')
-        .mapTo('/map-search');
+        .map((cmd: Command) => navigateTo[cmd.type])
+        .filter(path => path !== undefined);
 
     const sinks = extractSinks(componentSinks$, driverNames);
 
