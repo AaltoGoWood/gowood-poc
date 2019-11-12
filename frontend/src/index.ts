76a29e6994aa26312cb641e854deb1f8b6561c3c
pisketti <tero.kadenius@flowa.fi>
2019-11-12 12:13:34 +0200

    First try at adding three.js and a 3d model to the project

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -2,11 +2,13 @@ import { run } from '@cycle/run';
 import { getDrivers, wrapMain } from './drivers';
 import { Component } from './interfaces';
 import { App } from './components/app';
-
 import { initMap } from './components/map';
+import { foo, init3d } from './components/3d';
 
 const main: Component<any> = wrapMain(App);
-
+console.log('index.ts FTW');
+foo();
+init3d();
 run(main as any, getDrivers());
 
 // Init map once document is loaded
