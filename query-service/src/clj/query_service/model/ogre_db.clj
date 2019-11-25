cefbae58fbf8f982373c9358f7467c84b6d809de
Ari-Pekka Lappi <aplappi@gmail.com>
2019-11-25 14:02:14 +0200

    Initial version of query backend where data if fetched from Tinkerpop

---
+++ query-service/src/clj/query_service/model/ogre_db.clj
@@ -0,0 +1,170 @@
+(ns query-service.model.ogre-db
+  (:require [clojurewerkz.ogre.core :refer [open-graph traversal traverse value-map
+                                            match out has in select into-seq!
+                                            values as by V __] :as ogre])
+  (:import (org.apache.tinkerpop.gremlin.process.traversal Compare Operator Order P Pop SackFunctions$Barrier Scope Traversal)
+           (org.apache.tinkerpop.gremlin.structure Graph T Column VertexProperty$Cardinality Vertex)
+           (org.apache.tinkerpop.gremlin.structure.util GraphFactory)
+           (org.apache.tinkerpop.gremlin.structure.util.empty EmptyGraph)
+           (org.apache.tinkerpop.gremlin.process.traversal.dsl.graph GraphTraversal GraphTraversalSource)
+           (org.apache.tinkerpop.gremlin.driver.remote DriverRemoteConnection)
+           (org.apache.tinkerpop.gremlin.driver Cluster Cluster$Builder))
+  (:gen-class))
+
+(defn- attr->keyword [k]
+  (case k
+    "node-id" :id
+    "node-type" :type
+    (keyword k)))
+
+(declare normalize-object)
+(defn- normalize-value [k v]
+  (cond
+    (= k "coords") (clojure.string/split (first v) #",[ ]?")
+    (or (vector? v)
+        (instance? java.util.ArrayList v)) (first v)
+    (instance? org.apache.tinkerpop.gremlin.process.traversal.step.util.BulkSet v)
+    (vec (map normalize-object v))
+    (instance? java.util.LinkedHashMap v)
+    (normalize-object v)
+    :else v))
+; (normalize-value "coords" ["1, 2"])
+
+(defn normalize-object [obj]
+  (cond 
+    (and (instance? java.util.Optional obj) (not (.isPresent obj))) nil
+    (and (instance? java.util.Optional obj) (.isPresent obj)) (normalize-object (.get obj))
+    :else (into {} (map (fn [[k vl]] [(attr->keyword k) (normalize-value k vl)]) obj))))
+
+; (normalize-value "coord" ["1, 2"])
+; (normalize-value "foo" ["bar"])
+; (normalize-value "foo"
+;                  #{{"node-id" ["p125"], "node-type" ["plywood"]}
+;                    {"node-id" ["p124"], "node-type" ["plywood"]}
+;                    {"node-id" ["p123"], "node-type" ["plywood"]}})
+
+; (normalize-object {"node-id" ["746103"], "node-type" ["building"] "foobar" ["fkk"]})
+
+(defn get-graph []
+  (let [conf-file-url (clojure.java.io/resource "conf/remote-objects.yaml")
+        conf-file (clojure.string/replace (.toString conf-file-url) "file:/" "")
+        driverConnection (DriverRemoteConnection/using conf-file "g")
+        graph (EmptyGraph/instance)
+        g (.withRemote (traversal graph) driverConnection)]
+    g))
+
+(defn reset-graph []
+  (let [g (get-graph)]
+    (-> g V (ogre/drop) (.next))))
+
+(defn count-V []
+  (let [g (get-graph)]
+    (or 
+     (first (traverse g V (.count) (ogre/into-vec!))) 
+     0)))
+
+
+(defn count-E []
+  (let [g (get-graph)]
+    (or
+     (first (traverse g ogre/E (.count) (ogre/into-vec!)))
+     0)))
+
+(defn- entity [acc type id & [props]]
+  (-> acc
+      (.addV type) 
+      (ogre/property  "node-id" id)
+      (ogre/property "node-type" type)
+      (#(reduce (fn [acc, [k, v]] (ogre/property acc k v))
+                %
+                (or props {})))
+       (ogre/as (str type "/" id))))
+
+(defn- composed-of [acc fromNode toNode]
+  (-> acc
+      (.addE "composed-of")
+      (.from fromNode)
+      (.to toNode)))
+
+(defn init-poc-graph []
+  (let [g (get-graph)]
+    ;; (reset-graph)
+    (-> g
+        (entity "building" "746103")
+        (entity "plywood" "p123")
+        (entity "plywood" "p124")
+        (entity "plywood" "p125")
+        (entity "tree-trunk" "p123-1"  {"Species of Tree" "Pine"
+                                        "Trunk width" 75
+                                        "Timestamp" "2019-10-14T09:12:13.012Z"
+                                        "Length" 20
+                                        "Coords" "25.474293614, 65.0543745" })
+        (entity "tree-trunk" "p123-2")
+        (entity "tree-trunk" "p124-1")
+        (entity "tree-trunk" "p125-1")
+        (composed-of "building/746103" "plywood/p123")
+        (composed-of "building/746103" "plywood/p124")
+        (composed-of "building/746103" "plywood/p125")
+        (composed-of "plywood/p123" "tree-trunk/p123-1")
+        (composed-of "plywood/p123" "tree-trunk/p123-2")
+        (composed-of "plywood/p124" "tree-trunk/p124-1")
+        (composed-of "plywood/p125" "tree-trunk/p125-1")
+        (.next))))
+
+(defn get-nodes []
+  (let [g (get-graph)]
+    (traverse g V 
+      (ogre/value-map)
+      (ogre/into-vec!))))
+
+(defn get-node-with-components [node-type node-id]
+  (let [g (get-graph)]
+    (-> 
+     (traverse g V
+               (ogre/has node-type "node-id" node-id)
+               (ogre/as :attributes)
+               (out) (ogre/value-map) (ogre/aggregate :row)
+               (ogre/project "attributes" "rows")
+               (ogre/by (__ (select :attributes) (ogre/value-map)))
+               (ogre/by (__ (select :row)))
+               (.tryNext))
+     normalize-object)))
+
+;  (get-node-with-components "building" "746103")
+
+(defn get-edges []
+  (let [g (get-graph)]
+    (traverse g ogre/E
+      (ogre/into-vec!))))
+
+(defn apply-command [op body]
+  (let [{{id :id type :type} :from} body
+        data (get-node-with-components type id)
+        found? (some? data)
+        ]
+    (println (str "orge -> id: " id "; type: " type "; found " found?))
+    {:req {:op op :body body}
+     :result {:found found?
+              :data data}
+     :external-nodes []}))
+
+(apply-command nil {:from {:id "749103" :type "building"}})
+;; IN MEMORY DB GREMLIN TINKER GRAPH. EASIEST TO TEST FIRST
+; (def tinker-graph (open-graph {(Graph/GRAPH)
+;                                (.getName org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph)}))
+
+; (defn generate-toy-graph-into!
+;   "Generate the graph in createModern() into an existing graph"
+;   [graph]
+;   (org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory/generateModern graph))
+
+; (defn test-gremlin-tinkergraph []
+;   (generate-toy-graph-into! tinker-graph)
+;   (let [g (traversal tinker-graph)]
+;     (traverse g V (match
+;                    (__ (as :a) (out :created) (as :b))
+;                     (__ (as :b) (has :name "lop"))
+;                     (__ (as :b) (in :created) (as :c))
+;                     (__ (as :c) (has :age 29)))
+;               (select :a :c) (by :name)
+;               (into-seq!))))
---
+++ query-service/src/clj/query_service/routes/services.clj
@@ -0,0 +1,81 @@
+(ns query-service.routes.services
+  (:require
+
+   [reitit.swagger :as swagger]
+   [reitit.swagger-ui :as swagger-ui]
+   [reitit.ring.coercion :as coercion]
+   [reitit.coercion.spec :as spec-coercion]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [reitit.ring.middleware.multipart :as multipart]
+   [reitit.ring.middleware.parameters :as parameters]
+   [query-service.middleware.formats :as formats]
+   [query-service.middleware.exception :as exception]
+   [query-service.model.fake-db :as fake-db]
+   [query-service.model.ogre-db :as ogre-db]
+   [ring.util.http-response :refer :all]
+   [clojure.spec.alpha :as s]
+   [ring.util.response :refer [redirect]]
+   [clojure.java.io :as io]))
+
+(s/def ::operations #{"origins" "info-with-first-level-components"})
+
+(s/def ::type string?)
+(s/def ::id string?)
+(s/def ::from (s/keys :req-un [::type ::id]))
+(s/def ::operations-body (s/keys :opt-un [::from ]))
+
+(defn root-route [] 
+  ["/" {:get (constantly (redirect "/api/api-docs/" 302))}])
+
+(defn service-routes []
+  ["/api"
+   {:coercion spec-coercion/coercion
+    :muuntaja formats/instance
+    :swagger {:id ::api}
+    :middleware [;; query-params & form-params
+                 parameters/parameters-middleware
+                 ;; content-negotiation
+                 muuntaja/format-negotiate-middleware
+                 ;; encoding response body
+                 muuntaja/format-response-middleware
+                 ;; exception handling
+                 exception/exception-middleware
+                 ;; decoding request body
+                 muuntaja/format-request-middleware
+                 ;; coercing response bodys
+                 coercion/coerce-response-middleware
+                 ;; coercing request parameters
+                 coercion/coerce-request-middleware
+                 ;; multipart
+                 multipart/multipart-middleware]}
+
+   ;; swagger documentation
+   ["" {:no-doc true
+        :swagger {:info {:title "GoWood backend APIs"
+                         :description ""}}}
+
+    ["/swagger.json"
+     {:get (swagger/create-swagger-handler)}]
+
+    ["/api-docs/*"
+     {:get (swagger-ui/create-swagger-ui-handler
+            {:url "/api/swagger.json"
+             :config {:validator-url nil}})}]]
+   
+   ["/ping"
+    {:get (constantly (ok {:message "pong"}))}]
+
+   ["/query"
+    {:swagger {:tags ["query-api"]}}
+    ["/:operation"
+    {:post {:summary "Run named query starting from a node"
+            :parameters {:path {:operation ::operations }
+                         :body ::operations-body}
+            ;; :responses {200 {:body }}
+            :handler (fn [{:keys [parameters]}]
+                       (let [op (get-in parameters [:path :operation])
+                             cmd-body (get-in parameters [:body])]
+                         
+                         (ok (ogre-db/apply-command op cmd-body))))}}     
+     ]]])
+
