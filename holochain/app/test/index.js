e85cc19dfd4bedbf2f3c7ca85739df55016ec062
pisketti <tero.kadenius@flowa.fi>
2019-12-03 13:47:08 +0200

    Test uses the plywood zome

--- holochain/app/test/index.js
+++ holochain/app/test/index.js
@@ -1,13 +1,13 @@
 const { Orchestrator, Config } = require('@holochain/tryorama')
 
-const dnaHello = Config.dna(__dirname + '/../dist/holochain.dna.json', 'hello');
+const dnaPlywood = Config.dna(__dirname + '/../dist/holochain.dna.json', 'plywood');
 const dneFile = require('../dist/holochain.dna.json')
- 
+
 // Set up a Conductor configuration using the handy `Conductor.config` helper.
 // Read the docs for more on configuration.
 const mainConfig = Config.gen(
   {
-    hello: dnaHello,  // agent_id="hello", instance_id="hello", dna=dnaHello
+    plywood: dnaPlywood,  // agent_id="plywood", instance_id="plywood", dna=dnaPlywood
   },
   {
     // specify a bridge from chat to blog
@@ -30,16 +30,16 @@ const orchestrator = new Orchestrator()
 
 orchestrator.registerScenario("description of example test", async (s, t) => {
 
-  const { hello } = await s.players({hello: mainConfig })
-  await hello.spawn()
+  const { plywood } = await s.players({plywood: mainConfig })
+  await plywood.spawn()
   // Make a call to a Zome function
   // indicating the function, and passing it an input
-  const addr = await hello.call("hello", "hello", "create_my_entry", {"entry" : {"content":"sample content"}})
+  const addr = await plywood.call("plywood", "plywood", "create_my_entry", {"entry" : {"content":"sample content"}})
 
   // Wait for all network activity to
   await s.consistency()
 
-  const result = await hello.call("hello", "hello", "get_my_entry", {"address": addr.Ok})
+  const result = await plywood.call("plywood", "plywood", "get_my_entry", {"address": addr.Ok})
 
   // check for equality of the actual and expected results
   t.deepEqual(result, { Ok: { App: [ 'my_entry', '{"content":"sample content"}' ] } })
