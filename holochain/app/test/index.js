cc4f4f5dd1ae5fa921e8f7daedb3d65bed794f88
Ari-Pekka Lappi <aplappi@gmail.com>
2019-12-04 16:29:50 +0200

    Fix holochain gowood_key zome test

--- holochain/app/test/index.js
+++ holochain/app/test/index.js
@@ -1,7 +1,6 @@
 const { Orchestrator, Config } = require('@holochain/tryorama')
 
 const dnaGowoodKey = Config.dna(__dirname + '/../dist/holochain.dna.json', 'gowood_key');
-const dneFile = require('../dist/holochain.dna.json')
 
 // Set up a Conductor configuration using the handy `Conductor.config` helper.
 // Read the docs for more on configuration.
@@ -9,16 +8,7 @@ const mainConfig = Config.gen(
   {
     gowood_key: dnaGowoodKey,  // agent_id="plywood", instance_id="plywood", dna=dnaPlywood
   },
-  {
-    // specify a bridge from chat to blog
-    // bridges: [Config.bridge('bridge-name', 'chat', 'blog')],
-    // use a sim2h network (see conductor config options for all valid network types)
-    // network: {
-    //   type: 'sim2h',
-    //   sim2h_url: 'ws://localhost:9000',
-    // },
-    // etc., any other valid conductor config items can go here
-  }
+  { logger: { 'type': 'debug' } }
 )
 
 process.on('unhandledRejection', error => {
@@ -28,24 +18,32 @@ process.on('unhandledRejection', error => {
 
 const orchestrator = new Orchestrator()
 
-orchestrator.registerScenario("description of example test", async (s, t) => {
+orchestrator.registerScenario("test_1", async (s, t) => {
 
-  const { gowood_key } = await s.players({gowood_key: mainConfig })
+  const { gowood_key } = await s.players({gowood_key: mainConfig });
+  console.log('gowood_key', gowood_key);
+  console.log('gowood_key.instances', gowood_key.instances());
+
   await gowood_key.spawn()
-  // Make a call to a Zome function
-  // indicating the function, and passing it an input
-  const addr = await gowood_key.call("gowood_key", "gowood_key", "create_key_for_value ", {"value" : { "type":"plywood", "id": "p123" }})
 
-  // Wait for all network activity to
+  console.log('gowood_key.instances', gowood_key.instances());
+  const addr = await gowood_key.call("gowood_key", 
+     "gowood_key", 
+     "create_key_from_value", 
+      { "value" : { "type":"plywood", "id": "p123" }})
+
+
   await s.consistency()
 
-  const result = await gowood_key.call("gowood_key", "gowood_key", "get_value_from_key ", {"key": addr.Ok})
+  const result = await gowood_key.call("gowood_key", "gowood_key", "get_value_from_key", {"address": addr.Ok})
 
-  // check for equality of the actual and expected results
-  t.deepEqual(result, { Ok: { App: [ 'asset_identity', '{"type":"plywood", "id": "p123" }' ] } })
+  t.deepEqual(result, { Ok: { App: [ 'assets_identity', '{"type":"plywood","id":"p123"}' ] } })
 })
 
 orchestrator.run()
+  .catch(err => {
+    console.log('err', JSON.stringify(err))
+  })
   .then(res => {
     console.log(res);
   });
