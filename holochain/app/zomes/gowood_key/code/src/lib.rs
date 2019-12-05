fa3091029a934692965ab0023765a1169b8a6a22
Ari-Pekka Lappi <aplappi@gmail.com>
2019-12-05 17:00:14 +0200

    Implement functions create_signed_key_from_value and get_value_from_signed_key
    - At this point it possible to check if value should exist.
    - Next: Implement send->receive logic, so that user does not need to know which agend in holochain have the value

--- holochain/app/zomes/gowood_key/code/src/lib.rs
+++ holochain/app/zomes/gowood_key/code/src/lib.rs
@@ -12,10 +12,13 @@ extern crate holochain_json_derive;
 use hdk::{
     entry_definition::ValidatingEntryType,
     error::ZomeApiResult,
+    error::ZomeApiError,
 };
 use hdk::holochain_core_types::{
     entry::Entry,
     dna::entry_types::Sharing,
+    signature::Provenance,
+    signature::Signature,
 };
 
 use hdk::holochain_json_api::{
@@ -39,6 +42,13 @@ pub struct AssetsIdentity {
     r#type: String,
     id: String,
 }
+
+// #[derive(Serialize, Deserialize, Debug, DefaultJson,Clone)]
+// pub struct SignedKey {
+//     sub: String, 
+//     key: String,
+//     sig: String,
+// }
 
 #[zome]
 mod my_zome {
@@ -81,5 +91,43 @@ mod my_zome {
         let address = hdk::decrypt(key).map(Address::from).unwrap();
         hdk::get_entry(&address)
     }
+
+    
+    #[zome_fn("hc_public")]
+    fn create_signed_key_from_value(value: AssetsIdentity) -> ZomeApiResult<String> {
+        let entry = Entry::App("assets_identity".into(), value.into());
+        let address = hdk::commit_entry(&entry)?;
+        let encrypted_address = hdk::encrypt(address).unwrap();
+        let sig = hdk::sign(&encrypted_address).unwrap();
+
+        let sub = hdk::AGENT_ADDRESS.to_string();
+        let combined_key = format!("{}.{}.{}", sub, encrypted_address, sig);
+        Ok(combined_key)
+    }
+
+    #[zome_fn("hc_public")]
+    fn get_value_from_signed_key(key: String) -> ZomeApiResult<Option<Entry>> {
+        let parts: Vec<&str> = key.split(".").collect();
+        match &parts[..] { 
+            [sub, key, sig] => {
+                let address: Address = Address::from(sub.to_string());
+                let signature = Signature::from(sig.to_string());
+                let key_provenance = Provenance::new(address, signature);
+                let verify_result = hdk::verify_signature(key_provenance, &key.to_string());
+                if verify_result.is_ok() && verify_result.unwrap() {
+                    let address = hdk::decrypt(key.to_string()).map(Address::from).unwrap();
+                    hdk::get_entry(&address)
+                } else {
+                    let e = ZomeApiError::Internal("invalid signature".into());
+                    std::result::Result::Err(e)
+                }
+            }
+            _ => {
+                let e = ZomeApiError::Internal("invalid signature".into());
+                std::result::Result::Err(e)
+            }
+        }
+    }
+
 
 }
