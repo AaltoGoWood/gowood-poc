50764807304e5b508e202e7e3d5e7a4f1263cdd2
pisketti <tero.kadenius@flowa.fi>
2019-12-11 14:29:23 +0200

    Frontend conf

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,20 @@
 const path = require('path');
+require('dotenv').config();
 
 const appPath = (...names) => path.join(process.cwd(), ...names);
+
+console.log('FOO: ', process.env.FOO);
+console.log('DOTENV_CONFIG_FOO: ', process.env.FOO);
+
+console.log('QUERY_SERVICE_URL: ', process.env.QUERY_SERVICE_URL);
+console.log('DOTENV_CONFIG_QUERY_SERVICE_URL: ', process.env.QUERY_SERVICE_URL);
+
+console.log('NODE_ENV: ', process.env.NODE_ENV);
+
+const queryServiceUrl =
+    process.env.QUERY_SERVICE_URL || 'http://localhost:3000/';
+
+console.log('queryServiceUrl', queryServiceUrl);
 
 //This will be merged with the config from the flavor
 module.exports = {
